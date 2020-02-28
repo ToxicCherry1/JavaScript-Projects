c3061fae48e6f62d7070fffc6aec52982762dd55
ToxicCherry1 <53800605+ToxicCherry1@users.noreply.github.com>
2020-02-27 18:05:14 -0800

    Add completed Calculator project

---
+++ Calculator/js/calculator_javascript.js
@@ -0,0 +1,94 @@
+// Creates an object to keep track of values
+const Calculator = {
+    Display_Value: "0", // displays 0 on the screen
+    First_Operand: null, // holds the first operand of the expression
+    Wait_Second_Operand: false, // checks whether or not the second operand has been input
+    operator: null // holds the operator
+};
+
+// Modify values each time a button is clicked
+function Input_Digit(digit) {
+    const {Display_Value, Wait_Second_Operand} = Calculator;
+    if (Wait_Second_Operand === true) { // If Wait_Second_Operand is true, set Display_Value to pressed key
+        Calculator.Display_Value = digit;
+        Calculator.Wait_Second_Operand = false;
+    } else {
+        Calculator.Display_Value = Display_Value === "0" ? digit : Display_Value + digit; // Overwrites display value if its current value is 0
+    }
+}
+
+// Decimal points
+function Input_Decimal(dot) {
+    if (Calculator.Wait_Second_Operand === true) return; // Don't run the function if an operator button was last pressed
+    if (!Calculator.Display_Value.includes(dot)) { // Only add a decimal point if there isn't one already
+        Calculator.Display_Value += dot;
+    }
+}
+
+// Operators
+function Handle_Operator(Next_Operator) {
+    const {First_Operand, Display_Value, operator} = Calculator;
+    const Value_of_Input = parseFloat(Display_Value);
+    if (operator && Calculator.Wait_Second_Operand) { // If an operator already exists and Wait_Second_Operand is true, update operator and exit function
+        Calculator.operator = Next_Operator;
+        return;
+    }
+    if (First_Operand == null) {
+        Calculator.First_Operand = Value_of_Input;
+    } else if (operator) {
+        const Value_Now = First_Operand || 0;
+        const result = Perform_Calculation[operator](Value_Now, Value_of_Input);
+
+        Calculator.Display_Value = String(result);
+        Calculator.First_Operand = result;
+    }
+
+    Calculator.Wait_Second_Operand = true;
+    Calculator.operator = Next_Operator;
+}
+
+const Perform_Calculation = {
+    "/": (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+    "*": (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    "+": (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    "-": (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    "=": (First_Operand, Second_Operand) => Second_Operand
+};
+
+function Calculator_Reset() {
+    Calculator.Display_Value = "0";
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
+}
+
+function Update_Display() {
+    const display = document.querySelector(".calculator-screen");
+    display.value = Calculator.Display_Value;
+}
+
+Update_Display();
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", (event) => {
+    const {target} = event;
+    if (!target.matches("button")) {
+        return;
+    }
+    if (target.classList.contains("operator")) {
+        Handle_Operator(target.value);
+        Update_Display();
+        return;
+    }
+    if (target.classList.contains("decimal")) {
+        Input_Decimal(target.value);
+        Update_Display();
+    }
+    if (target.classList.contains("all-clear")) {
+        Calculator_Reset();
+        Update_Display();
+        return;
+    }
+
+    Input_Digit(target.value);
+    Update_Display();
+})
